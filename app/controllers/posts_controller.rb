2c7e2c7df26151f1aad93b7857dc39bf888b43f8
Maxine <maxinemeurer@yahoo.com>
2021-06-22 11:55:54 -0400

    create resource and posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+    before action :set_post, only: [:show, :update, :destroy]
+
+    def index
+        @posts = post.all 
+        render json: @posts
+    end
+
+    def show
+        render json: @post
+    end
+
+    def create
+        @post = post.new(post_params)
+
+        if @post.save
+            render json: @post, status: :created, location: @post 
+        else
+            render json: @post.errors, status: :unproccessable_entity
+        end
+    end
+
+    def destroy
+        @post.destroy
+    end
+
+    def set_post
+        @post = post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit(:title, :content, :link, :tag)
+    end
 end
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,35 @@
 class ResourcesController < ApplicationController
+    before action :set_resource, only: [:show, :update, :destroy]
+
+    def index
+        @resources = resource.all 
+        render json: @resources
+    end
+
+    def show
+        render json: @resource
+    end
+
+    def create
+        @resource = resource.new(resource_params)
+
+        if @resource.save
+            render json: @resource, status: :created, location: @resource 
+        else
+            render json: @resource.errors, status: :unproccessable_entity
+        end
+    end
+
+    def destroy
+        @resource.destroy
+    end
+
+    def set_resource
+        @resource = resource.find(params[:id])
+    end
+
+    def resource_params
+        params.require(:resource).permit(:name, :description, :link, :location)
+    end
+
 end
